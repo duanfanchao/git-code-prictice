004e45012178054dd51fdf84ccf7e001eb9e9f28
duanfanchao <Lebron150385@163.com>
2024-06-13 16:43:33 +0800

    ✨ feat(pages): 新增了app.vue文件

---
+++ src/pages/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app"></div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    components: {},
+    data () {
+        return {
+            
+        }
+    },
+    computed: {},
+    methods: {},
+    created () {},
+    mounted () {},
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
