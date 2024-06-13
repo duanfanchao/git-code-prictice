08df65717cee0b338073ef669d4d267ee6f67a98
duanfanchao <Lebron150385@163.com>
2024-06-13 17:11:08 +0800

    🐞 fix(api,components,styles): 新增了对应的文件

---
+++ src/components/NavItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-item"></div>
+</template>
+
+<script>
+export default {
+    name: 'nav-item',
+    components: {},
+    data () {
+        return {
+            
+        }
+    },
+    computed: {},
+    methods: {},
+    created () {},
+    mounted () {},
+}
+</script>
+
+<style lang="less" scoped>
+.nav-item {
+    width: 100%;
+}
+</style>
