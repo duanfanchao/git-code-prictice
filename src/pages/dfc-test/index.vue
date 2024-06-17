aadf671108da8fc629ec98e2edce34fa99d792a9
duanfanchao <Lebron150385@163.com>
2024-06-17 14:35:08 +0800

    ✨ feat(dfc-test): 新增模块

---
+++ src/pages/dfc-test/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dfc-test"></div>
+</template>
+
+<script>
+export default {
+    name: 'dfc-test',
+    components: {},
+    data () {
+        return {
+            
+        }
+    },
+    computed: {},
+    methods: {},
+    created () {},
+    mounted () {},
+}
+</script>
+
+<style lang="less" scoped>
+.dfc-test {
+    height: 100%;
+    width: 100%;
+}
+</style>
