f3cdf2283e63b92f2fd637b3847e46ff710e9b87
duanfanchao <Lebron150385@163.com>
2024-10-31 09:56:01 +0800

    🎉 init(app.vue): 初始化app页面

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<!--
+  * @projectName: 
+  * @desc: 
+  * @author: duanfanchao
+  * @date: 2024/06/20 10:00:00
+-->
+<template>
+  <div class="app">首页</div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+    data () {
+        return {
+            
+        }
+    },
+    computed: {},
+    methods: {},
+    created () {},
+    mounted () {},
+}
+</script>
+
+<style lang="less" scoped>
+.app {
+    height: 100%;
+    width: 100%;
+}
+</style>
