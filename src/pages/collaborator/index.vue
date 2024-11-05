86d6af9f26829ac921c79cf10342600cd4be5168
duan150385duan <duan150385duan@163.com>
2024-11-05 15:09:46 +0800

    ✨ feat(合作者页面): 静态页面

---
+++ src/pages/collaborator/index.vue
@@ -0,0 +1,30 @@
+<!--
+  * @projectName: 
+  * @desc: 
+  * @author: duanfanchao
+  * @date: 2024/06/20 10:00:00
+-->
+<template>
+	<div class="collaborator">合作者</div>
+</template>
+
+<script>
+export default {
+	name: "collaborator",
+	components: {},
+	data() {
+		return {};
+	},
+	computed: {},
+	methods: {},
+	created() {},
+	mounted() {},
+};
+</script>
+
+<style lang="less" scoped>
+.collaborator {
+    height: 100%;
+    width: 100%;
+}
+</style>
